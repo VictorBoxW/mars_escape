50979cf9a3699974fb9d3552309de0a091d48cd7
Victor Rateau <victorjohanko.rateau@studenti.unicam.it>
2026-06-13 18:10:50 +0200

    Add Castle class to hold all floors and the energy crystal

---
+++ app/src/main/java/model/Castle.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Castle {
+    private final List<Floor> floors;
+    private int currentFloorIndex;
+    private boolean energyCrystalTaken;
+
+    public Castle(List<Floor> floors) {
+        this.floors = new ArrayList<>(floors);
+    }
+
+    public List<Floor> getFloors() {
+        return Collections.unmodifiableList(floors);
+    }
+
+    public Floor getCurrentFloor() {
+        if (currentFloorIndex >= floors.size()) {
+            return null;
+        }
+
+        return floors.get(currentFloorIndex);
+    }
+
+    public int getCurrentFloorNumber() {
+        return Math.min(currentFloorIndex + 1, floors.size());
+    }
+
+    public int getFloorCount() {
+        return floors.size();
+    }
+
+    public boolean advanceFloor() {
+        if (currentFloorIndex < floors.size()) {
+            currentFloorIndex++;
+        }
+
+        return currentFloorIndex < floors.size();
+    }
+
+    public void takeEnergyCrystal() {
+        energyCrystalTaken = true;
+    }
+
+    public boolean isEnergyCrystalTaken() {
+        return energyCrystalTaken;
+    }
+}
