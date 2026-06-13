505f671d62785996547711e222e42b8c9d0a3627
Victor Rateau <victorjohanko.rateau@studenti.unicam.it>
2026-06-13 18:06:43 +0200

    Add ShieldItem class for extra protection

---
+++ app/src/main/java/model/ShieldItem.java
@@ -0,0 +1,17 @@
+package model;
+
+public class ShieldItem extends Item {
+    private final int shieldAmount;
+
+    public ShieldItem(String name, String description, int shieldAmount) {
+        super(name, description);
+        this.shieldAmount = shieldAmount;
+    }
+
+    @Override
+    public String interact(Player player) {
+        player.addShield(shieldAmount);
+        player.removeItem(this);
+        return player.getName() + " activated " + getName() + " and gained " + shieldAmount + " shield.";
+    }
+}
