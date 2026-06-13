4070911e69d7b04fa0524db0a7ae326c30468ee8
Victor Rateau <victorjohanko.rateau@studenti.unicam.it>
2026-06-13 18:06:24 +0200

    Add Consumable class for healing the astronaut

---
+++ app/src/main/java/model/Consumable.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Consumable extends Item {
+    private final int healingAmount;
+
+    public Consumable(String name, String description, int healingAmount) {
+        super(name, description);
+        this.healingAmount = healingAmount;
+    }
+
+    public int getHealingAmount() {
+        return healingAmount;
+    }
+
+    @Override
+    public String interact(Player player) {
+        int healed = player.heal(healingAmount);
+        player.removeItem(this);
+
+        if (healed == 0) {
+            return getName() + " had no effect.";
+        }
+
+        return player.getName() + " used " + getName() + " and recovered " + healed + " health.";
+    }
+}
